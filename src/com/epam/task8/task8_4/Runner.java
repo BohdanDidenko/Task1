7fa9f490a95966cd945558a7d74d2cb591adf1a2
BohdanDidenko <39033676+BohdanDidenko@users.noreply.github.com>
2018-12-12 01:53:47 +0200

    task 8.4

---
+++ src/com/epam/task8/task8_4/Runner.java
@@ -0,0 +1,57 @@
+package com.epam.task8.task8_4;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
+import java.util.concurrent.RecursiveTask;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Runner extends RecursiveTask<Long> {
+    private static final int SIZE_ARRAY = 1_000_000;
+    private static final int NUM_THREADS = 16;
+    long[] array;
+    int from = 0;
+    int to = SIZE_ARRAY;
+
+    public Runner(long[] array, int from, int to) {
+        this.from = from;
+        this.to = to;
+        this.array = array;
+    }
+
+    @Override
+    protected Long compute() {
+        if((to - from) < 20){
+            long count = 0;
+            for (int i = from; i < to; i++) {
+                count += array[i];
+            }
+            return count;
+        }
+        else{
+            int mid = (from + to) / 2;
+            Runner firstRunner = new Runner(array, from, mid);
+//            firstRunner.fork();
+            Runner secondRunner = new Runner(array, mid + 1, to);
+            invokeAll(firstRunner, secondRunner);
+//            long secondResult = secondRunner.compute();
+            return firstRunner.join() + secondRunner.join();
+//            return firstRunner.join() + secondResult;
+        }
+    }
+
+    public static void main(String[] args) {
+        long[] arrayForCount = new long[SIZE_ARRAY];
+        arrayForCount = initArrayRandomNumber(arrayForCount, 0, 100);
+        Runner runner = new Runner(arrayForCount, 0, 1_000_000);
+        ForkJoinPool pool = new ForkJoinPool(NUM_THREADS);
+        pool.invoke(runner);
+        System.out.println("Result = " + runner.join());
+    }
+
+    public static long[] initArrayRandomNumber(long[] array, int minBorder, int maxBorder){
+        for (int i = 0; i < array.length; i++) {
+            array[i] = ThreadLocalRandom.current().nextInt(minBorder, maxBorder + 1);
+        }
+        return array;
+    }
+}
---
+++ src/com/epam/task8/task8_5/Runner.java
@@ -0,0 +1,8 @@
+package com.epam.task8.task8_5;
+
+import java.util.Random;
+import java.util.concurrent.*;
+
+public class Runner {
+//
+}
